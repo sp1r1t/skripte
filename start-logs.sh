e383456d3f11074c4baf41c42bcfab8bd61b3b13
Julian Konrath <j.konrath@durchblicker.at>
2021-01-26 11:42:49 +0100

    fix username in start-logs

--- start-logs.sh
+++ start-logs.sh
@@ -3,7 +3,7 @@
 services=(website-next rkt-service-orchestrator sfdc transaction dblocal-auth server tarifs jabadabatool mg)
 
 for service in "${services[@]}"; do
-    innercmd="/home/j.konrath/.nvm/versions/node/v12.16.2/bin/r2de2 logs -s $service"
+    innercmd="/home/jkonrath/.nvm/versions/node/v12.16.2/bin/r2de2 logs -s $service"
     outercmd="bash -ic \"$innercmd\"; exec bash"
     echo $outercmd
     xfce4-terminal --title="$service" -e "$outercmd";
