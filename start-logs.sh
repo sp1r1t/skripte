baa389ac91ee375dff61dd538425c471fd886edf
Julian Konrath <j.konrath@durchblicker.at>
2021-01-28 17:00:11 +0100

    update start-logs: make node version 12 w/ dynamic minor version

--- start-logs.sh
+++ start-logs.sh
@@ -2,8 +2,17 @@
 
 services=(website-next rkt-service-orchestrator sfdc transaction dblocal-auth server tarifs jabadabatool mg)
 
+# load nvm
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
+nvm use 12
+
+r2=~/.nvm/versions/node/$(nvm current)/bin/r2de2
+echo 'using' $r2
+
 for service in "${services[@]}"; do
-    innercmd="/home/jkonrath/.nvm/versions/node/v12.16.2/bin/r2de2 logs -s $service"
+    innercmd="$r2 logs -s $service"
     outercmd="bash -ic \"$innercmd\"; exec bash"
     echo $outercmd
     xfce4-terminal --title="$service" -e "$outercmd";
